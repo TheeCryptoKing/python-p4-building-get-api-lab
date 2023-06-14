5cc9ca48116138d0c950a1ad9153aa4cc217cc76
kwamebrowne <theeecryptoking@gmail.com>
2023-06-13 20:10:06 -0500

    comprehension only, lab had to many problems

--- server/app.py
+++ server/app.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
@@ -19,20 +17,73 @@ def index():
     return '<h1>Bakery GET API</h1>'
 
 @app.route('/bakeries')
+# This line defines a route decorator for the /bakeries endpoint. When a GET request is made to this endpoint, the following function, bakeries(), will be executed.
 def bakeries():
-    return ''
+
+    bakeries = Bakery.query.all()
+    # This line executes a database query using SQLAlchemy's query interface to retrieve all the Bakery objects from the corresponding table in the database. It returns a list of Bakery objects.
+    bakeries_serialized = [bakery.to_dict() for bakery in bakeries]
+    # to_dict() method is called to serialize the object into a dictionary representation. The resulting dictionaries are collected into a new list called bakeries_serialized.
+    response = make_response(
+        jsonify(bakeries_serialized),
+        200
+    )
+    response.headers['Content-Type'] = 'application/json'
+    # response.headers['Content-Type'] = 'application/json' sets the value of the 'Content-Type' header in the HTTP response to 'application/json'.
+    # response.headers: This property provides access to the headers of the response. Headers are used to convey additional information about the response to the client.
+# 'Content-Type': This is the key of the header that specifies the type of content in the response.
+# 'application/json': This is the value assigned to the 'Content-Type' header. It indicates that the content of the response is in JSON format.
+    return response
+
+
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter_by(id=id).first()
+    # query: This is a method provided by SQLAlchemy that allows you to construct and execute queries on the database.
+    # filter_by(id=id): This is a filtering condition applied to the query. It specifies that the id attribute of the Bakery objects should match the provided id value.
+    # first(): This method is used to retrieve the first result from the query. Since id is expected to be unique, first() is commonly used to retrieve a single object based on the filtering condition.
+    bakery_serialized = bakery.to_dict()
+
+    response = make_response(
+        jsonify(bakery_serialized),
+        200
+    )
+    response.headers['Content-Type'] = 'application/json'
+    return response
+
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    by_price = BakedGood.query.order_by(BakedGood.price).all()
+    # order_by(BakedGood.price): This method is used to specify the ordering of the query results. It takes the price attribute of the BakedGood model as the ordering criterion.
+    # all(): This method executes the query and retrieves all the results as a list of BakedGood objects.
+    by_price_serialized = [bg.to_dict() for bg in by_price]
+    
+    response = make_response(
+        jsonify(by_price_serialized),
+        200
+    )
+    response.headers['Content-Type'] = 'application/json'
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).limit(1).first()
+    # order_by(BakedGood.price.desc()): This method is used to specify the ordering of the query results. It orders the BakedGood objects by their price attribute in descending order (desc() is used to indicate descending order).
+    # limit(1): This method limits the number of results returned by the query to 1. It ensures that only the highest-priced BakedGood is retrieved.
+    # first(): This method retrieves the first result from the query, which corresponds to the BakedGood object with the highest price.
+    most_expensive_serialized = most_expensive.to_dict()
+
+    response = make_response(
+        jsonify(most_expensive_serialized),
+        200
+    )
+    response.headers['Content-Type'] = 'application/json'
+    return response
+
+
+# Methods for selceting data filte, filter_by, order_by, select, query, scalars
 
 if __name__ == '__main__':
     app.run(port=555, debug=True)
--- server/models.py
+++ server/models.py
@@ -5,16 +5,52 @@ from sqlalchemy_serializer import SerializerMixin
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
+# This line sets the naming_convention parameter of the MetaData object to a dictionary representing the custom naming convention for foreign key constraints
+# this naming convention will generate foreign key constraint names in the format "fk_<table_name><column_0_name><referred_table_name>", where the placeholders are replaced with the appropriate names from the tables and columns involved in the foreign key relationship.
 
 db = SQLAlchemy(metadata=metadata)
+# establishing metadata and putting in a variable 
 
 class Bakery(db.Model, SerializerMixin):
     __tablename__ = 'bakeries'
 
+    serialize_rules = ('-baked_goods.bakery',)
+    # sterilization and set rules for sterilizing, this data wont populate Json  
+    # 'baked_goods.bakery': This represents the relationship between the current model (let's assume it's the Bakery model) and the related model (BakedGood). It specifies that the baked_goods attribute, which is a collection of related BakedGood objects, should be excluded from the serialized output. when the to_dict() method (or any serialization method) is called on an instance of the model class, it will exclude the baked_goods relationship from the serialized output. This can be useful when you want to control which attributes or relationships are included or excluded during serialization, providing a more focused or optimized representation of the data.
+    
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    baked_goods = db.relationship('BakedGood', backref='bakery')
+    # This line defines the relationship between the Bakery and BakedGood models.
+    # is the name of the related model/table that the relationship is established with.
+    # backref='bakery' creates a back reference in the BakedGood model. It adds a new attribute called 'bakery' to the BakedGood model, allowing access to the associated Bakery object.
+    # Allows bidimensional Data to flow between tables
+
+    def __repr__(self):
+        return f'<Bakery {self.name}>'
+
 
 class BakedGood(db.Model, SerializerMixin):
     __tablename__ = 'baked_goods'
 
+    serialize_rules = ('-bakery.baked_goods',)
+    # sterilization and set rules for sterilizing, this data wont populate Json  
+
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    price = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    # syntax needs to be like this for updates and creates
+
+    bakery_id = db.Column(db.Integer, db.ForeignKey('bakeries.id'))
+
+    def __repr__(self):
+        return f'<Baked Good {self.name}, ${self.price}>'
+    # representing function, represents the data in inside of retunn (validation i guess??)
     
+    #sterilizerMixin = converting data to Json
+    #Sterlize_rules = uses class names and sets rules for what will populate db and Json 
